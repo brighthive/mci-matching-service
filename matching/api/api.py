a34da6e3604f6105bc888584873288b719e7b665
reginafcompton <reginafcompton@gmail.com>
2019-05-08 17:07:21 -0500

    task: Implement procedural, 'dumb' approach to new algorithm

--- matching/api/api.py
+++ matching/api/api.py
@@ -7,7 +7,25 @@ from webargs.flaskparser import use_args
 
 from matching.common.util import compute_mci_threshold
 
-individual_args = {"first_name": fields.Str()}
+individual_args = {
+    'first_name': fields.Str(),
+    'date_of_birth': fields.Str(),
+    'last_name': fields.Str(),
+    'gender': fields.Str(),
+    'email': fields.Str(),
+    'telephone': fields.Str(),
+    'mailing_address_id': fields.Str(),
+    'ssn': fields.Str(),
+    # 'mailing_address': fields.Nested(
+    #     {
+    #         "address": fields.Str(), 
+    #         "city": fields.Str(), 
+    #         "state": fields.Str(), 
+    #         "postal_code": fields.Str(), 
+    #         "country": fields.Str(),
+    #     }
+    # ),
+}
 
 class ComputeMatch(Resource):
     @use_args(individual_args)
--- matching/common/util.py
+++ matching/common/util.py
@@ -12,9 +12,53 @@ def compute_mci_threshold(individual_args: dict):
         For the purpose of this exercise, we estimate our probability to be:
             first_name * last_name * email_address * address * dob * ssn
     """
+
     match = None
-    matched = False
     score = 0.0
+
+    potential_matches = session.query(individual).filter_by(
+        first_name=individual_args['first_name'],
+        date_of_birth=individual_args['date_of_birth'],
+    )
+
+    if potential_matches:
+        score = 0.4
+
+        filters = {
+            'last_name': individual_args.get('last_name', None),
+            'telephone': individual_args.get('telephone', None),
+            'gender_id': individual_args.get('gender', None),
+            'email_address': individual_args.get('email', None),
+            'mailing_address_id': individual_args.get('mailing_address_id', None),
+            'ssn': individual_args.get('ssn'),
+        }
+
+        import pdb; pdb.set_trace()
+        more_potential_matches = potential_matches.filter_by(**filters).all()
+
+        if len(more_potential_matches) == 1:
+            # Perfect match!
+            score += 0.6
+            match = more_potential_matches.first()
+        elif len(more_potential_matches) > 1:
+            # Too many matches returned. 
+            # Not enough data to determine a match.
+            match = None
+        else:
+            # thanks itertools! 
+            # https://stackoverflow.com/questions/11905573/getting-all-combinations-of-key-value-pairs-in-python-dict
+            from itertools import combinations
+            filter_combinations = list(map(dict, combinations(filters.items(), 5)))
+
+            for filter_c in filter_combinations:
+                match = potential_matches.filter_by(filter_c)
+
+                if match:
+                    score += 0.5
+        
+    return match, score
+
+
 
     weights = {
         'first_name': 0.1,
