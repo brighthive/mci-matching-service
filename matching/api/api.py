823b16487bfa939671ed72852d9ed5cd1b625ad4
reginafcompton <reginafcompton@gmail.com>
2019-05-08 22:12:55 -0500

    chore: Refactor initial new algorithm and add comments

--- matching/api/api.py
+++ matching/api/api.py
@@ -5,31 +5,22 @@ from flask_restful import Resource
 from webargs import fields
 from webargs.flaskparser import use_args
 
-from matching.common.util import compute_mci_threshold
+from matching.common.util import compute_match_with_score
 
 individual_args = {
     'first_name': fields.Str(),
     'date_of_birth': fields.Str(),
     'last_name': fields.Str(),
-    'gender': fields.Str(),
-    'email': fields.Str(),
+    'gender_id': fields.Str(),
+    'email_address': fields.Str(),
     'telephone': fields.Str(),
     'mailing_address_id': fields.Str(),
     'ssn': fields.Str(),
-    # 'mailing_address': fields.Nested(
-    #     {
-    #         "address": fields.Str(), 
-    #         "city": fields.Str(), 
-    #         "state": fields.Str(), 
-    #         "postal_code": fields.Str(), 
-    #         "country": fields.Str(),
-    #     }
-    # ),
 }
 
 class ComputeMatch(Resource):
     @use_args(individual_args)
     def post(self, args):
-        match, score = compute_mci_threshold(args)
+        mci_id, score = compute_match_with_score(args)
         
-        return {'score': score}, 201
+        return {'mci_id': mci_id, 'score': score}, 201
--- matching/common/util.py
+++ matching/common/util.py
@@ -1,90 +1,75 @@
+from itertools import combinations
+
 from matching.database import engine, individual, session
 
-def compute_score(weights: dict):
-    score = 0.0
-    for weight in weights.keys():
-        score += weights[weight]
-    return score
+def compute_match_with_score(individual_args: dict):
+    """
+    This function determines if incoming data for an Individual
+    can be reasonably matched with an existing entry. 
 
-def compute_mci_threshold(individual_args: dict):
-    """Compute the MCI Threshold for a given individual.
-    Notes:
-        For the purpose of this exercise, we estimate our probability to be:
-            first_name * last_name * email_address * address * dob * ssn
+    A match can have a score of 1.0, i.e., an existing entry meets the conditions 
+    described by the incoming Individual data (see: `filters` below). 
+
+    Or a match can have a score of 0.9, i.e., an existing entry matches
+    the `first_name` and `date_of_birth`, as well as five other fields, of the incoming Individual.
+
+    Implicit in this logic: the `first_name` and `last_name` are weighted at 0.2 (or 0.4 together)
+    and each remaining field are weighted at 0.1. Note! The precise weights for these fields
+    will be fine tuned, as we come to understand the data better.
+
+    :param individual_args: a dict of Individual data that includes, at minimum, a
+    `first_name` and `date_of_birth`.
+
+    :returns: an Individual and a score of the match likelihood (or None) 
     """
 
-    match = None
-    score = 0.0
-
-    potential_matches = session.query(individual).filter_by(
-        first_name=individual_args['first_name'],
-        date_of_birth=individual_args['date_of_birth'],
-    )
-
-    if potential_matches:
-        score = 0.4
-
-        filters = {
-            'last_name': individual_args.get('last_name', None),
-            'telephone': individual_args.get('telephone', None),
-            'gender_id': individual_args.get('gender', None),
-            'email_address': individual_args.get('email', None),
-            'mailing_address_id': individual_args.get('mailing_address_id', None),
-            'ssn': individual_args.get('ssn'),
-        }
-
-        import pdb; pdb.set_trace()
-        more_potential_matches = potential_matches.filter_by(**filters).all()
-
-        if len(more_potential_matches) == 1:
-            # Perfect match!
-            score += 0.6
-            match = more_potential_matches.first()
-        elif len(more_potential_matches) > 1:
-            # Too many matches returned. 
-            # Not enough data to determine a match.
-            match = None
-        else:
-            # thanks itertools! 
-            # https://stackoverflow.com/questions/11905573/getting-all-combinations-of-key-value-pairs-in-python-dict
-            from itertools import combinations
-            filter_combinations = list(map(dict, combinations(filters.items(), 5)))
-
-            for filter_c in filter_combinations:
-                match = potential_matches.filter_by(filter_c)
-
-                if match:
-                    score += 0.5
-        
-    return match, score
-
-
-
-    weights = {
-        'first_name': 0.1,
-        'last_name': 0.1,
-        'mailing_address_id': 0.2,
-        'date_of_birth': 0.2,
-        'ssn': 0.4
-    }
+    mci_id = None
+    score = None
+    
+    
+    ssn = individual_args.get('ssn')
+    if ssn: 
+        ssn_last_four_digits = ssn[-4:]
 
     filters = {
-        'first_name': individual_args.get('first_name', None),
-        'last_name': individual_args.get('last_name', None),
-        'mailing_address_id': individual_args.get('mailing_address_id', None),
-        'date_of_birth': individual_args.get('date_of_birth', None),
-        'ssn': individual_args.get('ssn', None),
+        'last_name': individual_args.get('last_name'),
+        'telephone': individual_args.get('telephone'),
+        'gender_id': individual_args.get('gender_id'),
+        'email_address': individual_args.get('email_address'),
+        'mailing_address_id': individual_args.get('mailing_address_id'),
+        'ssn': individual_args.get('ssn'),
+        # 'ssn': like("%s{}".format(ssn_last_four_digits)) if ssn_last_four_digits else ssn,
+        # TODO: how to query for last four digits of SSN, and move the logic further upstream
     }
 
-    while not matched and len(filters.keys()) > 0:
-        match = session.query(individual).filter_by(**filters).first()
-        if match:
-            matched = True
-            score = compute_score(weights)
-        else:
-            for key in weights.keys():
-                del(weights[key])
-                del(filters[key])
-                break
 
-    return match, score
+    exact_match = session.query(individual).filter_by(
+        first_name=individual_args['first_name'],
+        date_of_birth=individual_args['date_of_birth']
+    ).filter_by(**filters)
+
+    if exact_match.first():
+        # Note: the sample data has duplicates (e.g., Susan Hall)
+        # So, an exact_match queryset can contain more than one entry.
+        # Is it better to just match with one? rather than create a third duplicate?
+        mci_id = exact_match.first().mci_id
+        score = 1.0
+    else:
+        # thanks itertools! 
+        # https://stackoverflow.com/questions/11905573/getting-all-combinations-of-key-value-pairs-in-python-dict
+        filter_combinations = list(map(dict, combinations(filters.items(), 5)))
+        import pdb; pdb.set_trace()
+
+        for filter_combo in filter_combinations:
+            strong_match = session.query(individual).filter_by(
+                first_name=individual_args['first_name'],
+                date_of_birth=individual_args['date_of_birth'],
+            ).filter_by(**filter_combo)
+
+            if strong_match.first():
+                # TO TEST can strong_match have more than one object?
+                mci_id = strong_match.first().mci_id
+                score = 0.9
+                break
+        
+    return mci_id, score
