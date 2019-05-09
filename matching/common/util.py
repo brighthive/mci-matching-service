90be93eb473293b1d07b88e837536fcba68abf1c
reginafcompton <reginafcompton@gmail.com>
2019-05-09 16:09:36 -0500

    refactor: Consider all combinations of filters, so that the MCI can consider low threshold matches

--- matching/common/util.py
+++ matching/common/util.py
@@ -4,14 +4,9 @@ from matching.database import engine, individual, session
 
 def compute_match_with_score(individual_args: dict):
     """
-    This function determines if incoming data for an Individual
-    can be reasonably matched with an existing entry. 
-
-    A match can have a score of 1.0, i.e., an existing entry meets the conditions 
-    described by the incoming Individual data (see: `filters` below). 
-
-    Or a match can have a score of 0.9, i.e., an existing entry matches
-    the `first_name` and `date_of_birth`, as well as five other fields, of the incoming Individual.
+    This function determines (1) if incoming data for an Individual
+    can be reasonably matched with an existing entry, and (2) the
+    score assigned to the likelihood of matching. 
 
     Implicit in this logic: the `first_name` and `last_name` are weighted at 0.2 (or 0.4 together)
     and each remaining field are weighted at 0.1. Note! The precise weights for these fields
@@ -26,10 +21,10 @@ def compute_match_with_score(individual_args: dict):
     mci_id = None
     score = None
     
-    
     ssn = individual_args.get('ssn')
     if ssn: 
         ssn_last_four_digits = ssn[-4:]
+
 
     filters = {
         'last_name': individual_args.get('last_name'),
@@ -42,33 +37,26 @@ def compute_match_with_score(individual_args: dict):
         # TODO: how to query for last four digits of SSN, and move the logic further upstream
     }
 
-
-    exact_match = session.query(individual).filter_by(
+    potential_matches = session.query(individual).filter_by(
         first_name=individual_args['first_name'],
         date_of_birth=individual_args['date_of_birth']
-    ).filter_by(**filters)
+    )
 
-    if exact_match.first():
-        # Note: the sample data has duplicates (e.g., Susan Hall)
-        # So, an exact_match queryset can contain more than one entry.
-        # Is it better to just match with one? rather than create a third duplicate?
-        mci_id = exact_match.first().mci_id
-        score = 1.0
-    else:
-        # thanks itertools! 
-        # https://stackoverflow.com/questions/11905573/getting-all-combinations-of-key-value-pairs-in-python-dict
-        filter_combinations = list(map(dict, combinations(filters.items(), 5)))
+    if potential_matches.first():
+        score = 0.4
+        list_of_combos = []
+        # Find all filter combinations.
+        # Reference: https://stackoverflow.com/questions/11905573/getting-all-combinations-of-key-value-pairs-in-python-dict
+        for i in range(len(filters), 0, -1):
+            list_of_combos += list(map(dict, combinations(filters.items(), i)))
 
-        for filter_combo in filter_combinations:
-            strong_match = session.query(individual).filter_by(
-                first_name=individual_args['first_name'],
-                date_of_birth=individual_args['date_of_birth'],
-            ).filter_by(**filter_combo)
+        for combo in list_of_combos:
+            match = potential_matches.filter_by(**combo)
 
-            if strong_match.first():
-                # TO TEST can strong_match have more than one object?
-                mci_id = strong_match.first().mci_id
-                score = 0.9
+            if match.first():
+                # TO TEST can a `match` have more than one Individual?
+                mci_id = match.first().mci_id
+                score += len(combo) * 0.1
                 break
-        
+
     return mci_id, score
