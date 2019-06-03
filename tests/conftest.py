24c08db82383ab28c05332f1620b45f9467520f5
reginafcompton <reginafcompton@gmail.com>
2019-06-03 16:42:22 -0500

    task: Write a couple unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,18 @@
-import time
-from io import BytesIO
 import os
 import subprocess
+import time
+from io import BytesIO
+from time import sleep
 
 import docker
 import pytest
+from flask import Flask
+from flask_migrate import Migrate, upgrade
+
+from mci_database import db
+
+MAX_RETRIES = 10
+SLEEP = 2
 
 class PostgreSQLContainer(object):
     """A PostgreSQL Container Object.
@@ -22,107 +30,109 @@ class PostgreSQLContainer(object):
         self.docker_client = docker.from_env()
         self.postgres_image = "postgres:11.2"
         self.container_name = "postgres_test"
+        self.postgres_user = "brighthive"
+        self.postgres_password = "test_password"
+        self.postgres_db = "mci_dev"
 
     def setup_postgres_container(self):
         """Setup Docker PostgreSQL container.
         Spins up a Docker PostgreSQL container for testing.
         """
-        # cleanup old containers
         try:
-            container_to_clean = self.docker_client.containers.get(self.container_name)
-            container_to_clean.stop()
-            container_to_clean.remove()
+            self.teardown_postgres_container()
         except docker.errors.NotFound:
             print('Container not found.')
-        # download Docker PostgreSQL image for unit testing only
-        try:
-            print('Launching Docker PostgreSQL Container...')
-            self.docker_client.images.pull(self.postgres_image)
-        except Exception as err:
-            print(err)
-            print('Failed to retrieve PostgreSQL image!')
+        except docker.errors.APIError:
+            sleep(SLEEP)
+            self.teardown_postgres_container()
 
-        # launch Docker PostgreSQL image for unit testing only
-        db_environment = [
-            'POSTGRES_USER={}'.format('brighthive'),
-            'POSTGRES_PASSWORD={}'.format('test_password'),
-            'POSTGRES_DB={}'.format('mci_dev')
-        ]
+        self.docker_client.images.pull(self.postgres_image)
 
-        try:
-            self.docker_client.containers.run(
-                self.postgres_image,
-                detach=True,
-                auto_remove=True,
-                name=self.container_name,
-                ports={'5432/tcp': '5436'},
-                environment=db_environment,
-                tmpfs={'/tmp/mci_models.tar': ''},
-            )
+        self.docker_client.containers.run(
+            self.postgres_image,
+            detach=True,
+            auto_remove=True,
+            name=self.container_name,
+            ports={'5432/tcp': '5436'},
+            environment=[
+                'POSTGRES_USER={}'.format(self.postgres_user),
+                'POSTGRES_PASSWORD={}'.format(self.postgres_password),
+                'POSTGRES_DB={}'.format(self.postgres_db)
+            ],
+            tmpfs={'/tmp/mci_models.tar': ''},
+        )
+        
+        # Borrowed from: https://github.com/pallets/flask/blob/master/tests/conftest.py#L61
+        app = Flask("flask_test", root_path=os.path.dirname(__file__))
 
+        migrate = Migrate(app, db)
+        
+        app.config.from_mapping(
+            TESTING = True,
+            SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(
+                self.postgres_user,
+                self.postgres_password,
+                'localhost',
+                5436,
+                self.postgres_db)
+        )
+        
+        applied_migrations = False
+        retries = 0
 
-            import os
-            from flask import Flask
-            from flask_migrate import upgrade
-            from flask_migrate import Migrate
+        with app.app_context():
+            # The migrations repo resides in the virtual env.
+            # Specifically, Pipenv installs the mci-database repo in the `src` directory,
+            # since the Pipfile marks it as "editable."
+            path_to_virtual_env = os.environ['VIRTUAL_ENV']
+            migrations_dir = os.path.join(
+                path_to_virtual_env, 'src', 'mci-database', 'mci_database', 'db', 'migrations')
 
-            # Made possible by: pipenv install ../master-client-index/dist/mci-1.0.tar.gz
-            from mci import db
-            
-            # Borrowed from: https://github.com/pallets/flask/blob/master/tests/conftest.py#L61
-            app = Flask("flask_test", root_path=os.path.dirname(__file__))
+            while retries < MAX_RETRIES and applied_migrations is False:
+                print('Attempting to apply migrations ({} of {})...'.format(
+                    retries + 1, MAX_RETRIES))
+                try:
+                    # apply the migrations
+                    upgrade(directory=migrations_dir)
+                    applied_migrations = True
+                except Exception:
+                    retries += 1
+                    sleep(SLEEP)
 
-            migrate = Migrate(app, db)
-            
-            app.config.from_mapping(
-                TESTING = True,
-                SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(
-                    'brighthive',
-                    'test_password',
-                    'localhost',
-                    5436,
-                    'mci_dev')
-            )
-            
-            with app.app_context():
-                
+        # A container with tables! Ready for population with fixtures!
+        psql_container = self.docker_client.containers.get('postgres_test')    
 
-                path_to_virtual_env = os.environ['VIRTUAL_ENV']
-                # N.b, this path makes sense on my machine, only...
-                migrations_dir = os.path.join(
-                            path_to_virtual_env, 'lib/python3.7/site-packages', 'mci', 'db', 'migrations')
-
-                # TODO: Wait? Or while loop?
-                import time
-                time.sleep(2)
-
-                upgrade(directory=migrations_dir)
-
-            # A container with tables! Ready for population with fixtures!
-            psql_container = self.docker_client.containers.get('postgres_test')    
-
-
-
-        except Exception as err:
-            print(err)
-            print('Unable to start container...')
 
     def teardown_postgres_container(self):
         """Teardown Dockerr PostgreSQL container.
         Spins down the Docker PostgreSQL testing container.
         """
-        print('Tearing Down Docker PostgreSQL Container...')
-        try:
-            container = self.docker_client.containers.get(self.container_name)
-            container.stop()
-        except Exception:
-            print('Unable to stop container...'.format(self.container_name))
+        print('Stopping and removing Docker PostgreSQL Container...')
+        container_to_clean = self.docker_client.containers.get(
+            self.container_name)
+        container_to_clean.stop()
+        container_to_clean.remove()
 
 
 @pytest.fixture()
 def psql_docker():
     """Database container."""
     return PostgreSQLContainer()
+
+@pytest.fixture()
+def individual_data():
+    individual_data = {
+        'mci_id': '1qaz2wsx3edc',
+        'ssn': '123456789',
+        'first_name': 'George',
+        'last_name': 'Handel',
+        'middle_name': 'Frideric',
+        'date_of_birth': '1985-01-01',
+        'email_address': 'handel@hotmail.com',
+        'telephone': '123-456-7890',
+    }
+
+    return individual_data
 
 
 @pytest.fixture
--- tests/test_matching.py
+++ tests/test_matching.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+
 
 def test_the_truth():
 
@@ -8,3 +10,29 @@ def test_get_endpoint(client, app):
     response = client.get('/compute-match')
 
     assert response.status_code == 200
+
+def test_user_match_ninety(client, app, individual_data):
+    from matching.database import engine, individual
+
+    engine.execute(individual.insert(), **individual_data)
+
+    response = client.post('/compute-match', data=json.dumps(individual_data))
+
+    assert response.status_code == 201
+    assert response.get_json()['mci_id'] == individual_data['mci_id']
+    assert response.get_json()['score'] == 0.9
+
+
+def test_user_match_forty(client, app, individual_data):
+    from matching.database import engine, individual
+    # http: // flask.pocoo.org/docs/1.0/patterns/sqlalchemy/
+    engine.execute(individual.insert(), **individual_data)
+
+    poor_match = {
+        'date_of_birth': individual_data['date_of_birth'],
+        'last_name': individual_data['last_name']
+    }
+    response = client.post('/compute-match', data=json.dumps(poor_match))
+
+    assert response.status_code == 201
+    assert response.get_json()['score'] == 0.4
